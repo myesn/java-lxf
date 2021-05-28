a962ffb91db8c18f4651100e37bc78ddc0d04cfa
myesn <myesn@foxmail.com>
2021-05-28 16:17:36 +0800

    🎉 创建泛型学习项目

---
+++ 5.泛型/generic/src/com/company/myesn/Main.java
@@ -0,0 +1,9 @@
+package com.company.myesn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 泛型
+        // https://www.liaoxuefeng.com/wiki/1252599548343744/1255945193293888
+    }
+}
